25fdbf8705e882dfe1f060a02429d8ecc69a423d
yaoyonstudio <yaozekun_999@163.com>
2016-03-03 03:53:20 +0800

    'data'

--- data/sliderData.php
+++ data/sliderData.php
@@ -5,21 +5,6 @@
 	header('Access-Control-Allow-Headers:x-requested-with,content-type');  
 
 	if ($_SERVER["REQUEST_METHOD"] === "GET") {
-	
-		// $sliderImgArr = array();
-		
-
-		// $sliderImgArr[0] = {id:1,sliderImgUrl:'http://www.test.com/slider1.jpg',title:'slider1'};
-		// $sliderImgArr[1] = {id:2,sliderImgUrl:'http://www.test.com/slider1.jpg',title:'slider1'};
-		// $sliderImgArr[2] = {id:3,sliderImgUrl:'http://www.test.com/slider1.jpg',title:'slider1'};
-		// $sliderImgArr[3] = {id:4,sliderImgUrl:'http://www.test.com/slider1.jpg',title:'slider1'};
-
-			// {id:1,sliderImgUrl:'http://www.test.com/slider1.jpg',title:'slider1'},
-			// {id:2,sliderImgUrl:'http://www.test.com/slider2.jpg',title:'slider2'},
-			// {id:3,sliderImgUrl:'http://www.test.com/slider3.jpg',title:'slider3'},
-			// {id:4,sliderImgUrl:'http://www.test.com/slider4.jpg',title:'slider4'}
-		// );
-
 
 		$sliderImgArr = array(
 			array("id"=>1,"sliderImgUrl"=>"http://www.test.com/slider1.jpg","title"=>"slider1"),
@@ -28,10 +13,8 @@
 			array("id"=>4,"sliderImgUrl"=>"http://www.test.com/slider4.jpg","title"=>"slider4")
 		);
 
-
 		echo json_encode($sliderImgArr);
 
 	}
-
 
 ?>
